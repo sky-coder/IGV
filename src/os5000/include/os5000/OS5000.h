29c942f2c4121b83e86e6317efe2d916f1f2e19a
Jihang <jihang2014@my.fit.edu>
2016-07-02 21:58:56 -0400

    can use library to read raw orientation data now

--- src/os5000/include/os5000/OS5000.h
+++ src/os5000/include/os5000/OS5000.h
@@ -4,7 +4,10 @@
 #include <tf/transform_datatypes.h>
 
 // cpp includes
-// #include <math.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+
 
 #ifndef COVARIANCE_INITIALIZER //TODO change the name
 #define COVARIANCE_INITIALIZER
@@ -15,6 +18,7 @@
 
 #endif // COVARIANCE_INITIALIZER
 
+
 #ifndef IGV_OS5000_H
 #define IGV_OS5000_H
 
@@ -24,15 +28,24 @@ class OS5000
 		OS5000();
 		~OS5000();
 
+		// Part 1
+		void getOrientation();
+		float getYaw();
+		float getPitch();
+		float getRoll();
+		
 		// Part
+		void setupSerialConnection();
 		void initializeIMU();
 
 	private:
 		sensor_msgs::Imu ImuMsg;
+		serial::Serial OSPort;
 
-		float yaw, pitch, roll, temperature;
-
-		// float OrientationElement, InvalidElement;
+		std::string RawData; //TODO get a better identifier
+		std::stringstream Orientation;
+		unsigned int position, length;
+		float yaw, pitch, roll;
 };
 
 # endif // IGV_OS5000_H
